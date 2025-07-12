2191ab3973f178583ec5fe61bed6b9196f7765a1
Christiana Nguyen <christiana@coastpay.com>
2025-07-11 23:50:39 -0600

    fix routes import in index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@
 
 import 'dotenv/config';
 import express from 'express';
-import { registerRoutes } from './routes';
+import { registerRoutes } from "./routes.js";
 
 const app = express();
 app.use(express.json());
